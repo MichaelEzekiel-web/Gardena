3c32fb5991013097bd5369e650c1a055b561e8df
MichaelEzekiel-wed <michaelezekiel724@gmail.com>
2023-05-23 17:10:15 +0000

    create commit

--- gardena.js
+++ gardena.js
@@ -120,3 +120,164 @@ const direction = e.target.dataset.direction
 // if(direction === "next") slider.next()
 // if(direction === "prev") slider.prev()
 })
+
+
+// HEADER ***
+
+const menuOpen = document.querySelector('.menuOpen');
+const menuIcon = document.querySelector('.menu');
+const dropdownMenu = document.querySelector('.dropdownMenu');
+
+menuOpen.onclick = function () {
+  dropdownMenu.classList.toggle('open')
+  const isOpen = dropdownMenu.classList.contains('open')
+
+  menuIcon.classList = isOpen ? '.menuClose img' : '.menuOpen img'
+}
+
+
+// Tabs Box ***
+
+const catergoriesBox = document.querySelector(".catergoriesList"),
+allTags = catergoriesBox.querySelectorAll(".tag"),
+arrowIcons = document.querySelectorAll(".icon img");
+
+let isDragging = false;
+
+const handleIcons = (scrollVal) => {
+  let maxScrollableWidth = catergoriesBox.scrollWidth - catergoriesBox.clientWidth;
+  arrowIcons[0].parentElement.style.display = scrollVal <= 0 ? "none" : "flex";
+  arrowIcons[1].parentElement.style.display = maxScrollableWidth - scrollVal <= 1 ? "none" : "flex";
+};
+
+arrowIcons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    // if clicked icon is left, reduce 350 from catergoriesBox scrollLeft else add
+    let scrollWidth = (catergoriesBox.scrollLeft += icon.id === "left" ? -340 : 340);
+    handleIcons(scrollWidth);
+  });
+});
+
+allTags.forEach((tag) => {
+  tag.addEventListener("click", () => {
+    catergoriesBox.querySelector(".active").classList.remove("active");
+    tag.classList.add("active");
+  });
+});
+
+const dragging = (e) => {
+  if (!isDragging) return;
+  catergoriesBox.classList.add("dragging");
+  catergoriesBox.scrollLeft -= e.movementX;
+  handleIcons(catergoriesBox.scrollLeft);
+};
+
+const dragStop = () => {
+  isDragging = false;
+  catergoriesBox.classList.remove("dragging");
+};
+
+catergoriesBox.addEventListener("mousedown", () => (isDragging = true));
+catergoriesBox.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+
+/* ------------------------ Watermark (Please Ignore) ----------------------- */
+const createSVG = (width, height, className, childType, childAttributes) => {
+  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+
+  const child = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    childType
+  );
+
+  for (const attr in childAttributes) {
+    child.setAttribute(attr, childAttributes[attr]);
+  }
+
+  svg.appendChild(child);
+
+  return { svg, child };
+};
+
+document.querySelectorAll(".generate-button").forEach((button) => {
+  const width = button.offsetWidth;
+  const height = button.offsetHeight;
+
+  const style = getComputedStyle(button);
+
+  const strokeGroup = document.createElement("div");
+  strokeGroup.classList.add("stroke");
+
+  const { svg: stroke } = createSVG(width, height, "stroke-line", "rect", {
+    x: "0",
+    y: "0",
+    width: "100%",
+    height: "100%",
+    rx: parseInt(style.borderRadius, 10),
+    ry: parseInt(style.borderRadius, 10),
+    pathLength: "30"
+  });
+
+  strokeGroup.appendChild(stroke);
+  button.appendChild(strokeGroup);
+
+  const stars = gsap.to(button, {
+    repeat: -1,
+    repeatDelay: 0.5,
+    paused: true,
+    keyframes: [
+      {
+        "--generate-button-star-2-scale": ".5",
+        "--generate-button-star-2-opacity": ".25",
+        "--generate-button-star-3-scale": "1.25",
+        "--generate-button-star-3-opacity": "1",
+        duration: 0.3
+      },
+      {
+        "--generate-button-star-1-scale": "1.5",
+        "--generate-button-star-1-opacity": ".5",
+        "--generate-button-star-2-scale": ".5",
+        "--generate-button-star-3-scale": "1",
+        "--generate-button-star-3-opacity": ".5",
+        duration: 0.3
+      },
+      {
+        "--generate-button-star-1-scale": "1",
+        "--generate-button-star-1-opacity": ".25",
+        "--generate-button-star-2-scale": "1.15",
+        "--generate-button-star-2-opacity": "1",
+        duration: 0.3
+      },
+      {
+        "--generate-button-star-2-scale": "1",
+        duration: 0.35
+      }
+    ]
+  });
+
+  button.addEventListener("pointerenter", () => {
+    gsap.to(button, {
+      "--generate-button-dots-opacity": "1",
+      duration: 0.5,
+      onStart: () => {
+        setTimeout(() => stars.restart().play(), 500);
+      }
+    });
+  });
+
+  button.addEventListener("pointerleave", () => {
+    gsap.to(button, {
+      "--generate-button-dots-opacity": "0",
+      "--generate-button-star-1-opacity": ".25",
+      "--generate-button-star-1-scale": "1",
+      "--generate-button-star-2-opacity": "1",
+      "--generate-button-star-2-scale": "1",
+      "--generate-button-star-3-opacity": ".5",
+      "--generate-button-star-3-scale": "1",
+      duration: 0.15,
+      onComplete: () => {
+        stars.pause();
+      }
+    });
+  });
+});
